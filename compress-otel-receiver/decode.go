0f9f1e17d288b4402c327f249f6c9553e920ea8e
beet233 <liuyixiao0821@gmail.com>
2024-03-04 13:23:23 +0800

    Decode string pool

--- compress-otel-receiver/decode.go
+++ compress-otel-receiver/decode.go
@@ -11,4 +11,26 @@ func Decode(def *model.Definition, in io.Reader) (*model.Value, error) {
 		return nil, err
 	}
 	reader := NewDataReader(data)
+	var result model.Value
+	// decode stringPool
+	stringPoolSize, err := reader.readLeb128Int()
+	if err != nil {
+		return nil, err
+	}
+	stringPool := make([]string, 0, stringPoolSize)
+	for i := 0; i < stringPoolSize; i++ {
+		stringLen, err := reader.readLeb128Int()
+		if err != nil {
+			return nil, err
+		}
+		string, err := reader.readString(stringLen)
+		if err != nil {
+			return nil, err
+		}
+		stringPool = append(stringPool, string)
+		// fmt.Println(string)
+	}
+	// decode valuePools
+	// valuePools := make(map[string][]model.Value)
+	return &result, nil
 }
